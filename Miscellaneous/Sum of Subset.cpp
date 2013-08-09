7d3c6796410c5cfe769556279d6b87c132fe152c
rezwan4029 <rezwan.maruf.ndc09@gmail.com>
2013-08-08 22:05:30 -0700

    backtrack

---
+++ Miscellaneous/Sum of Subset.cpp
@@ -0,0 +1,42 @@
+#define SZ 107
+int n , m , w[SZ], cnt ;
+bool Vis[100];
+
+void SumOfSubset(int s,int k,int r)
+{
+   Vis[k] = true ;
+   if(s + w[k] == m ){
+       printf("Subset solution %d:\t",++cnt);
+       printf("(");
+       forab(i,0,k) if( Vis[i] ) printf(" %d",w[i]);
+       printf(" )\n");
+   }
+   else if( s + w[k] + w[k+1] <= m )
+        SumOfSubset( s + w[k] , k+1, r - w[k] );
+
+   if( ( s + r - w[k] >= m ) && ( s + w[k+1] ) <= m ){
+      Vis[k] = 0;
+      SumOfSubset(s , k + 1 , r - w[k] );
+   }
+}
+
+int main(){
+    scanf("%d",&n) ;
+    int tot = 0 ;
+    rep(i,n) {
+        scanf("%d",&w[i]) ;
+        tot += w[i] ;
+    }
+    sort(w,w+n);
+    scanf("%d",&m);
+    if(tot < m || w[0] > m){
+            printf("No solution\n");
+    }
+    else{
+        SumOfSubset(0,0,tot);
+        if(!cnt) printf("No solution\n");
+    }
+}
+
+You have n elements in W array . You have to print all possible solution that can make (m) using those elements .
+
