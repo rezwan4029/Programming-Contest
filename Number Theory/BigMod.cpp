f3c8edc14dd5209d44607e82e196172e6b2f36e0
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-08-14 15:39:02 +0600

    added BigMod

---
+++ Number Theory/BigMod.cpp
@@ -0,0 +1,24 @@
+//compute b^p%m
+
+ll BigMod(ll  b,ll  p ,ll  m ){
+     ll  ret = 1 ;
+     while(p)  {
+        if(p&1) ret = (ret * b ) % m ;
+        p >>= (1ll) , b = (b*b)%m ;
+     }
+    return ret ;
+}
+
+//compute b^p
+
+ll Pow(ll  b,ll  p){
+    ll  ret = 1;
+    while(p){
+        if(p&1) ret *= b ;
+        p >>= (1ll) , b *= b ;
+    }
+    return ret ;
+}
+
+
+// Complexity log(n)
