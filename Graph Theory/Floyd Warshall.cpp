05ad81a57b09ea7360cf2baf582effb5efefccff
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-08-28 05:24:33 -0700

    all pair shortest path

---
+++ Graph Theory/Floyd Warshall.cpp
@@ -0,0 +1,40 @@
+// Graph Represnted as adjacancy matrix
+
+#define SZ 105
+
+int n , d[SZ][SZ] , pred[SZ][SZ];
+
+void Reset(){
+    For(i,n) For(j,n){
+        if(i == j ) d[i][j] = 0 ;
+        else d[i][j] = INF ;
+        pred[i][j] = 0 ;
+    }
+}
+
+void Warshall(){
+    For(k,n)For(i,n)For(j,n){
+            if(d[i][j] > d[i][k] + d[k][j] ){
+                d[i][j] = d[i][k] + d[k][j] ;
+                pred[i][j] = k ;
+            }
+    }
+}
+
+void PrintPath(int i,int j){
+    if(pred[i][j] == 0 ) printf("%d %d -> ",i,j);
+    else{
+        PrintPath(i,pred[i][j]);
+        PrintPath(pred[i][j],j);
+    }
+}
+
+void Input(){
+    n = II ;
+    int edges = II ;
+    Reset();
+    rep(i,edges){
+        int u = II , v = II , w = II ;
+        d[u][v] = d[v][u] = w ;
+    }
+}
