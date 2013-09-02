d31efd6c7882acf7a86a457a8635d62434dae9a7
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-09-02 05:22:53 -0700

    Complexity : O(n)

--- Data Structure/Histogram.cpp
+++ Data Structure/Histogram.cpp
@@ -1,18 +1,17 @@
-#define mx 2002
+#define SZ 2007
+int Hist[SZ];
 
-int Histogram(int Hist[],int n)
+int Histogram(int n)
 {
     // Hist has values in 1..to..n
-    int top = 1, stk[mx], ret = 0;
+    int top = 1, stk[SZ], ret = 0;
     Hist[0] = stk[0] = Hist[++n] = 0;
-    FOR(i,n)
-    {
-        while( top > 1 && Hist[ stk[top-1] ] >= Hist[i] )
-        {
-             ret = max( ret, Hist[ stk[top-1] ] * ( i - stk[ top-2 ] - 1 ) );
+    FOR(i,n){
+        while( top > 1 && Hist[ stk[ top - 1 ] ] >= Hist[i] ){
+             ret = max( ret, Hist[ stk[top - 1 ] ] * ( i - stk[ top - 2 ] - 1 ) );
              top--;
         }
-        stk[ top++ ] = i;
+        stk[ top++ ] = i ;
     }
     return ret;
 }
