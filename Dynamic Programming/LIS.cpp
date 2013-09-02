b213caa8be19f483820ef3914c7bc2a5cbc727b0
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-09-02 12:54:49 -0700

    Complexity : N log(K)

---
+++ Dynamic Programming/LIS.cpp
@@ -0,0 +1,22 @@
+const int SZ = 100010 ;
+int n , arr[SZ] , dp[SZ];
+
+int lis() {
+
+    int mxLen = 1;
+    dp[0] = arr[0];
+    For(i,n-1){
+        int curr = arr[i] , last = dp[ mxLen - 1 ] ;
+        if ( last < curr ) {
+            dp[ mxLen++ ] = curr;
+        }
+        else if( last > curr ) {
+            int *pos = lower_bound(dp , dp + mxLen , curr) ;
+            if( *pos > curr) *pos = curr;
+        }
+    }
+    return mxLen ;
+    // for solution print the dp upto mxLen
+}
+
+ // for longest decreasing subarruence, multiply elements by -1
