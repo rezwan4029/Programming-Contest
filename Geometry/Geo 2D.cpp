49dc54905a4aa4404190e32fa000031d12e4e9dc
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-10-09 16:39:58 +0600

    Update Geo 2D.cpp

--- Geometry/Geo 2D.cpp
+++ Geometry/Geo 2D.cpp
@@ -43,21 +43,6 @@ bool LinesCollinear(point a, point b, point c, point d) {
 }
 
 
-// determine if line segment from a to b intersects with
-// line segment from c to d
-bool SegmentsIntersect(point a, point b, point c, point d) {
-  if (LinesCollinear(a, b, c, d)) {
-    if (dist2(a, c) < EPS || dist2(a, d) < EPS ||
-      dist2(b, c) < EPS || dist2(b, d) < EPS) return true;
-    if (dot(c-a, c-b) > 0 && dot(d-a, d-b) > 0 && dot(c-b, d-b) > 0)
-      return false;
-    return true;
-  }
-  if (cross(d-a, b-a) * cross(c-a, b-a) > 0) return false;
-  if (cross(a-c, d-c) * cross(b-c, d-c) > 0) return false;
-  return true;
-}
-
 // compute intersection of line passing through a and b
 // with line passing through c and d, assuming that unique
 // intersection exists; for segment intersection, check if
