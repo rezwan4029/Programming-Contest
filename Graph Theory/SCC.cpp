9cb912ddbc1b5b7903f60438cc1536e2481687b6
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-10-26 15:10:36 +0600

    Strongly Connected Component ( Kosaraju )

---
+++ Graph Theory/SCC.cpp
@@ -0,0 +1,64 @@
+//all are 1 indexed Graph
+
+const int MX = 10005; // nodes number
+vi adj[MX],Radj[MX],order,Comp;
+bool used[MX];
+int newCompId[MX];
+int N , M;
+
+void dfs1(int u) {
+    used[u] = true ;
+    forstl(it,adj[u]) if( !used[ *it ] ) dfs1( *it );
+    order.pb(u); // entrying in topolist acording to time to complete task
+}
+
+void dfs2(int u , int scc ){
+    newCompId[u] = scc ;
+    used[u] =  true ;
+    Comp.pb(u);
+    forstl(it,Radj[u]) if( !used[ *it ] ) dfs2( *it , scc );
+}
+
+void Reset(){
+    For(i,N){
+        adj[i].clear();
+        Radj[i].clear();
+    }
+    ms(newCompId,false);
+    ms(used,false);
+    order.clear();
+}
+
+void Input(){
+    cin >> N >> M ;
+    rep(i,M){
+        int u = II , v = II ;
+        adj[u].pb(v);
+        Radj[v].pb(u);
+    }
+}
+
+void Zip(){
+    reverse(all(order));
+    int scc = 0 ;
+    forstl(it,order){
+        int v = *it ;
+        if( !used[ v ] ){
+            scc++;
+            Comp.clear();
+            dfs2(v,scc);
+            printf("No.%d SCC components are ->  ",scc);
+            forstl(u,Comp) printf(" %d",*u);
+            puts("");
+        }
+    }
+}
+
+int main(){
+    //freopen("in.txt", "r", stdin);
+    Input();
+    For(i,N) if( !used[i] ) dfs1(i);
+    ms(used,false);
+    Zip();
+    Reset();
+}
