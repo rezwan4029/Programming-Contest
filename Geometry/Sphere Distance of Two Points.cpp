296efa4df0a2e15450045024e3d6af73297bfa4a
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-11-18 22:12:29 +0600

    Update Sphere Distance of Two Points.cpp

--- Geometry/Sphere Distance of Two Points.cpp
+++ Geometry/Sphere Distance of Two Points.cpp
@@ -7,5 +7,10 @@ double spherical_distance(double lat1,double lon1,double lat2,double lon2) {
        double a = pow((sin(dlat/2)),2) + cos(lat1) * cos(lat2) * pow(sin(dlon/2), 2);
        double c = 2 * atan2(sqrt(a), sqrt(1-a));
        double d = R * c;
+       double e = sqrt(2.*R*R *(1-cos(c)));
        return d+EPS;
+       //return round(d-e+EPS); 
+       //if need the difference in the distance between the two points 
+       //following the great circle route around the surface of the earth 
+       //and following the straight line through the earth
 }
