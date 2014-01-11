36aaf772dfbba62bdf5c02eb6796089b78574873
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-01-11 13:34:07 +0600

    Range Minimum Query [Complexity : preprocess O(n)]

--- Data Structure/RMQ.cpp
+++ Data Structure/RMQ.cpp
@@ -3,18 +3,16 @@ const int MXLG = 17;
 int dp[MX][MXLG];
 int a[MX] , N ;
 int preprocess() {
-    for( int i = 0 ; i < N ; i++ ) dp[i][0] = i ;
-    for( int j = 1 ; ( 1 << j ) <= N ; j++ ){
-        for( int i = 0 ; i + ( 1 << j ) - 1 < N ; i++ )
-            if( a[ dp[i][j - 1] ] < a[ dp[ i + ( 1 << ( j - 1 ) )][j - 1] ])
-                 dp[i][j] = dp[i][ j - 1];
-            else dp[i][j] = dp[ i + ( 1 << ( j - 1 ) )][j - 1];
+    for( int i = 0 ; i < N ; i++ ) dp[i][0] = a[i]; // init the 0th level s = 1 , k = 0
+    for( int s = 2 , k = 1 ; s <= N ; k++ ) { // powering up each level with s *= 2 , and k ++
+        for( int i = 0 ; i + s <= N ; i++ )
+            dp[i][k] = min( dp[i][k-1] , dp[i+s/2][k-1] );
+        s *= 2 ;
     }
 }
 int rmq(int i , int j ) {
-    int k = ( int )log2( j - i + 1 );
-    if( a[ dp[i][k] ] <= a[ dp[ j - (1 << k) + 1 ][k] ] )
-        return a[ dp[i][k] ];
-    else
-        return a[ dp[ j - (1 << k) + 1 ][k] ];
+    int range = j - i + 1 , s = 1, k = 0 ;
+    for( s = 1 , k = 0 ; s * 2 <= range ; k++ , s *= 2 );
+    int ans = min( dp[i][k] , dp[i+range-s][k]);
+    return ans;
 }
