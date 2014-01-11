d871989cfe0114141bea28b81a20f1aba31ff5cf
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-01-11 19:12:25 +0600

    Update Sparse Table.cpp

--- Data Structure/Sparse Table.cpp
+++ Data Structure/Sparse Table.cpp
@@ -1,4 +1,4 @@
-/** !-D Range Queries **/
+/** 1-D Range Queries **/
 
 const int MX = 1e5 + 7 ;
 const int MXLG = 17;
@@ -19,6 +19,7 @@ int rmq(int i , int j ) {
     return ans;
 }
 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /** 2-D Square Queries **/
 
@@ -50,5 +51,42 @@ int rmq(int i , int j , int range ){
     ans = max( ans , dp[i+range-s][j][k] );
     ans = max( ans , dp[i][j+range-s][k] );
     ans = max( ans , dp[i+range-s][j+range-s][k]);
+    return ans;
+}
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/** Rectangle Queries **/
+
+const int MX = 256 ;
+int LogTable[MX];
+int dp[9][9][MX][MX];
+int N , a[MX][MX] ;
+
+void preprocess()
+{
+    for( int i = 2 ; i < MX; i++ ) LogTable[i] = LogTable[i>>1]+1;
+    for(int i = 0 ; i < N ; i++)
+        for(int j = 0 ; j < N ; j++)
+            dp[0][0][i][j] = a[i][j];
+    for(int i = 0 ; i <= LogTable[N] ;i++)
+        for(int j = 0 ; j <= LogTable[N] ;++j ){
+            if(i == 0 && j == 0 ) continue;
+            for(int x = 0 ; x + (1<<j) <= N ; x++)
+                for(int y = 0 ; y + (1<<i) <= N ; y++)
+                    if( i == 0 )
+                        dp[i][j][x][y]= max(dp[i][j-1][x][y],dp[i][j-1][x+(1<<(j-1))][y]);
+                    else
+                        dp[i][j][x][y]= max(dp[i-1][j][x][y],dp[i-1][j][x][y+(1<<(i-1))]);
+        }
+}
+
+int rmq(int r1,int c1,int r2,int c2) {
+    int w = LogTable[c2-c1+1];
+    int h = LogTable[r2-r1+1];
+    int ans = dp[w][h][r1][c1] ;
+    ans = max( ans , dp[w][h][r1][c2-(1<<w)+1] );
+    ans = max( ans , dp[w][h][r2-(1<<h)+1][c1] );
+    ans = max( ans , dp[w][h][r2-(1<<h)+1][c2-(1<<w)+1] );
     return ans;
 }
