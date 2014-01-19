4e6ef9444b32972e26c2462ada755c7e8c6f40ce
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-01-19 17:13:41 +0600

    Update Articulation Point and BCC.cpp

--- Graph Theory/Articulation Point and BCC.cpp
+++ Graph Theory/Articulation Point and BCC.cpp
@@ -3,39 +3,36 @@
  * undirected graphs
  */
 
-#define SZ 10001
+#define MX 10001
 
-int N,M,counter, seen[SZ], low[SZ];
-vector<int>adj[SZ],stk;
+int N,M,counter, seen[MX], low[MX];
+vector<int>adj[MX],stk;
 
-void dfs(int u, int par)
-{
-    int kids = 0, art = 0;
+void dfs(int u, int par) {
+    int children = 0, art = 0;
     low[u] = seen[u] = ++counter;
-
     stk.pb(u);
-
-    forstl(it,adj[u])
-    {
+    forstl(it,adj[u]) {
         int v = *it;
         if (v == par) continue;
-        if (seen[v] == 0)
-        {
+        if ( !seen[v] ) {
             dfs(v,u);
+            children++;
             low[u] = min(low[u], low[v]);
-            art |= (low[v] >= seen[u]);
-            kids++;
+            if( low[v] >= seen[u] ) {
+                printf("%d is an articulation point.\n",u);
+            }
             if (low[v] == seen[v]) printf("Edge (%d, %d) is a bridge.\n", u, v);
         }
-        else
-        {
+        else {
             low[u] = min(low[u], seen[v]);
         }
     }
 
-    if (par == -1) art = (kids >= 2);
-
-    if (art) printf("Vertex %d is an articulation point.\n", u);
+    /** root of DFS is an articulation point if it has more than 1 child */
+    if (par == -1 && children > 1 ) {
+        printf("%d is an articulation point.\n",u);
+    }
 
     if (low[u] == seen[u]) {
         printf("Biconnected component:");
@@ -58,11 +55,11 @@ void Reset(){
 
 int main(){
     cin >> N >> M;
+    Reset();
     rep(i,M){
         int x = II , y = II ;
         adj[x].push_back(y);
         adj[y].push_back(x);
     }
     For(i,N)if(seen[i] == 0) dfs(i, -1);
-    Reset();
 }
