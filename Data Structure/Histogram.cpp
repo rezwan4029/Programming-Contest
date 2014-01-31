1bec8dc6e406395ffae5b19f7e4f8ea212b90f71
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-02-01 02:33:30 +0600

    Update Histogram.cpp

--- Data Structure/Histogram.cpp
+++ Data Structure/Histogram.cpp
@@ -4,17 +4,15 @@ int Histogram() {
     stack<int>Stk;
     int Ans = 0 , pos = 0 , top , Area_With_Top ;
     while( pos < N ){
-        if( Stk.empty() || H[ Stk.top() ] <= H[pos] ) Stk.push(pos++);
+        if( Stk.empty() || H[ Stk.top() ] <= H[ pos ] ) Stk.push( pos++ );
         else {
-            top = Stk.top() ;
-            Stk.pop();
+            top = Stk.top() ; Stk.pop();
             Area_With_Top = H[top] * ( Stk.empty() ? pos : pos - Stk.top() - 1 );
             Ans = max( Ans , Area_With_Top );
         }
     }
     while( !Stk.empty() ){
-        top = Stk.top();
-        Stk.pop();
+        top = Stk.top(); Stk.pop();
         Area_With_Top = H[top] * ( Stk.empty() ? pos : pos - Stk.top() - 1 );
         Ans = max( Ans , Area_With_Top );
     }
